5dba5f2638c0476fb626ef4609ff414e584be0fd
Alina Kostiuk <alina.hariaha@gmail.com>
2021-12-14 19:08:28 +0200

    Create results form.

--- src/components/PaymentForm.jsx
+++ src/components/PaymentForm.jsx
@@ -1,52 +1,59 @@
-import { Form, Field } from "react-final-form";
+import {Form, Field} from "react-final-form";
+import {useState} from "react";
 
 export const PaymentForm = () => {
+    const [formResults, setFormResults] = useState("");
     const formData = {};
     const onSubmit = (values, form) => {
-        window.alert(JSON.stringify(values, 0, 2));
+        let result = JSON.stringify(values, 0, 2);
+        setFormResults(prevState => prevState + result)
         form.restart();
     };
-    //Validators
+
+    // Validators.
     const required = (value) => (value ? undefined : "Please fill out this field");
     const mustBeString = (value) => (value.match(/^[A-Za-z]+$/) ? undefined : "Please input alphabet characters only");
-    const mustBeNumber = (value) => (isNaN(value) ? "Must be a number" : undefined);
+    const mustBeNumber = (value) => (isNaN(value) ? "Please input digits only" : undefined);
     const mustBeLength16 = (value) => ((value.length === 16) ? undefined : "Must be 16 digits");
     const mustBeLength3 = (value) => ((value.length === 3) ? undefined : "Must be 3 digits");
-    const validateEmail = (value) => (((/\S+@\S+\.\S+/).test(value)) ? undefined : "Must be valid email")
+    const validateEmail = (value) => {
+        if (value) {
+            return (/\S+@\S+\.\S+/).test(value) ? undefined : "Please input valid email (example: info@geniusee.com)";
+        }
+    }
     const composeValidators = (...validators) => (value) => {
         return validators.reduce((error, validator) => error || validator(value), undefined);
     }
-    //Classes
+
+    // Classes.
     const getInputClass = (meta, classOfInput = "form-control") => {
         let classes = classOfInput;
-
         if (meta.error && meta.touched) {
             classes += ' is-invalid';
         }
-
         return classes;
     }
 
     return (
-        <div className="vh-100 d-flex justify-content-center align-items-center">
+        <div className="vh-100 d-flex flex-column justify-content-center align-items-center">
             <div className="payment-form container col-md-6">
                 <h2 className="payment-form__title">Payment form</h2>
                 <Form
                     onSubmit={onSubmit}
                     initialValues={formData}
-                    render={({ handleSubmit, form, submitting, pristine, values }) => (
+                    render={({handleSubmit, form, submitting, pristine, values}) => (
                         <form
                             method="POST"
                             action="https://httpbin.org/post"
                             onSubmit={handleSubmit}
                             className="row g-3 needs-validation payment-form__form"
-                            novalidate
+                            noValidate
                         >
                             <Field
                                 name="firstName"
                                 validate={composeValidators(required, mustBeString)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-6">
                                         <label form="validationFirstName" className="form-label">First Name*</label>
                                         <input
@@ -65,7 +72,7 @@ export const PaymentForm = () => {
                                 name="lastName"
                                 validate={composeValidators(required, mustBeString)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-6">
                                         <label form="validationLastName" className="form-label">Last Name*</label>
                                         <input
@@ -84,7 +91,7 @@ export const PaymentForm = () => {
                                 name="country"
                                 validate={composeValidators(required, mustBeString)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-6">
                                         <label form="validationCountry" className="form-label">Country*</label>
                                         <input
@@ -103,7 +110,7 @@ export const PaymentForm = () => {
                                 name="address"
                                 validate={composeValidators(required)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-6">
                                         <label form="validationAddress" className="form-label">Address*</label>
                                         <input
@@ -122,7 +129,7 @@ export const PaymentForm = () => {
                                 name="creditCard"
                                 validate={composeValidators(required, mustBeNumber, mustBeLength16)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-10">
                                         <label form="validationCreditCard" className="form-label">Credit Card*</label>
                                         <input
@@ -142,7 +149,7 @@ export const PaymentForm = () => {
                                 name="cvv2Code"
                                 validate={composeValidators(required, mustBeNumber, mustBeLength3)}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-2">
                                         <label form="validationCvv2Code" className="form-label">CVV2*</label>
                                         <input
@@ -160,9 +167,9 @@ export const PaymentForm = () => {
 
                             <Field
                                 name="email"
-                                validate={composeValidators(validateEmail)}
+                                validate={validateEmail}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-12">
                                         <label form="validationEmail" className="form-label">Email for a receipt</label>
                                         <input
@@ -180,7 +187,7 @@ export const PaymentForm = () => {
                                 name="agreement"
                                 validate={required}
                             >
-                                {({ input, meta }) => (
+                                {({input, meta}) => (
                                     <div className="col-md-12">
                                         <div className="form-check">
                                             <label form="validationAgreement" className="form-check-label">
@@ -191,8 +198,9 @@ export const PaymentForm = () => {
                                                     className={getInputClass(meta, "form-check-input")}
                                                     id="validationAgreement"
                                                 />
-                                                Agree with terms of use
-                                                <div className="invalid-feedback">You must agree before submitting.</div>
+                                                Agree with terms of use*
+                                                <div className="invalid-feedback">You must agree before
+                                                    submitting.</div>
                                             </label>
 
                                         </div>
@@ -212,6 +220,14 @@ export const PaymentForm = () => {
                     )}
                 />
             </div>
+            {formResults && (
+                <div className="form-results container col-md-6">
+                    <code lang="json" className="form-results__code">
+                        Results: {formResults}
+                    </code>
+                </div>
+            )
+            }
         </div>
     );
 }
